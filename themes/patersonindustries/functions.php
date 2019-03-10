c9731ef73acf0c49cc937f8201323fe02559f72c
aliyahchance <aliyahachance@gmail.com>
2019-03-10 13:15:01 -0400

    Add main footer elements

--- themes/patersonindustries/functions.php
+++ themes/patersonindustries/functions.php
@@ -1,25 +1,24 @@
 <?php
-
-//Adds featured images to posts
 add_theme_support('post-thumbnails');
 
-//Add Widget Areas inside {}
-function blank_widgets_init(){
+register_sidebar(array(
+  'name' => ('Footer Left'),
+  'id' => 'footer-left',
+  'description' => 'widget for footer left area',
+  'before_widget' => '<div class="footer-left">',
+  'after_widget' => '</div>',
+  'before_title' => '<h2>',
+  'after_title' => '</h2>'
+));
 
-//START WIDGET AREAS
+register_sidebar(array(
+  'name' => ('Footer Right'),
+  'id' => 'footer-right',
+  'description' => 'widget for footer right area',
+  'before_widget' => '<div class="footer-right">',
+  'after_widget' => '</div>',
+  'before_title' => '<h2>',
+  'after_title' => '</h2>'
+  ));
 
-
-
-
-
-
-
-
-
-
-
-
-
-//END WIDGET AREAS
-
- ?>
+  ?>
