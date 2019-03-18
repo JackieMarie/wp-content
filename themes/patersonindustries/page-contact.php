1c4dc2e932cec7f326e482b248e4135b28455971
Alana Saavedra <alasaave@gmail.com>
2019-03-17 19:15:34 -0500

    Add HTML form to contact page (no functionality)

--- themes/patersonindustries/page-contact.php
+++ themes/patersonindustries/page-contact.php
@@ -24,6 +24,23 @@
        ?>
     </article>
     <div class="twelve columns">
+      <form name="contactform" method="post" action="send_form_email.php">
+        <fieldset>
+          <input type="text" name="name" maxlength="50" size="30" placeholder="Name or Company">
+          <input type="email" name="email" maxlength="50" size="30" placeholder="Email Address">
+          <input type="tel" name="telephone" maxlength="30" size="30" placeholder="Phone Number">
+          <select name="reasons">
+              <option value="interest" disabled selected>I am interested in....</option>
+              <option value="construction">Facility Construction</option>
+              <option value="maintenance">Maintenance</option>
+              <option value="turn">Turn Around and Shut Down</option>
+              <option vale="other">Other (please explain below)</option>
+              <option value="hi">Just sayin' hi!</option>
+          </select>
+          <textarea name="comments" maxlength="2500" cols="25" rows="6" placeholder="Leave your message here..."></textarea>
+          <input type="submit" value="Submit">
+        </fieldset>
+      </form>
       <?php dynamic_sidebar('call-to-action'); ?>
     </div>
   </div>
