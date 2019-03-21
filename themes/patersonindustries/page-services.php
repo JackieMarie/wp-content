3d43e19de245359be1b75222cb7c24b76d47e8cb
Jackie Culbreath <jmc081964@gmail.com>
2019-03-21 14:34:09 -0400

    Updated services page with WP loop.

--- themes/patersonindustries/page-services.php
+++ themes/patersonindustries/page-services.php
@@ -11,6 +11,19 @@
       </div>
     </div>
   </header>
+  <section class="row">
+    <article class="twelve columns">
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post(); ?>
+            <h2><?php the_title(); ?></h2>
+            <?php the_content();
+          } //ends while loop
+        } //ends if statement
+       ?>
+    </article>
+  </section>
   <!--Our Services Section  -->
   <main class="row">
     <div class="seven columns">
